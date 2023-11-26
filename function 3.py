52a7e3fedf433e02e06a6154a8a81de2f9aba6ca
IkaDhealisna <148429512+IkaDhealisna@users.noreply.github.com>
2023-11-26 18:06:30 +0700

    function 3.py

---
+++ function 3.py
@@ -0,0 +1,27 @@
+#nama : Ika Dhealisna Listyani 
+#no absen : 12
+#kelas : XI TKJ 1
+
+
+
+def hitung_pangkat(bilangan, eksponen):
+
+    hasil = bilangan ** eksponen
+
+    return hasil
+
+
+
+# Mengambil input bilangan dan eksponen dari pengguna
+
+bilangan = float(input("Masukkan bilangan: "))
+
+eksponen = int(input("Masukkan eksponen: "))
+
+
+
+# Memanggil fungsi dan mencetak hasilnya
+
+hasil_pangkat = hitung_pangkat(bilangan, eksponen)
+
+print(f"{bilangan}^{eksponen} = {hasil_pangkat}")
